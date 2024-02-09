18f71a09c6247788e5fac67e264f7b7f6770c54c
sansrp358 <sanskrutirpaitwar@gmail.com>
2024-02-09 16:55:56 +0530

    adding the final code

---
+++ src/books.js
@@ -0,0 +1,38 @@
+export const books = [
+  {
+    id: 1,
+    author: "Brianna Wiest",
+    img: "https://m.media-amazon.com/images/I/61xivWmExiL._AC_UY327_FMwebp_QL65_.jpg",
+    title: "The Mountain is You",
+  },
+  {
+    id: 2,
+    author: "Morgan Housel",
+    img: "https://m.media-amazon.com/images/I/61-hMfd7NGL._AC_UY327_FMwebp_QL65_.jpg",
+    title: "The Psychology of Money",
+  },
+  {
+    id: 3,
+    author: "Colleen Hoover",
+    img: "https://m.media-amazon.com/images/I/813aV273-rL._AC_UY327_FMwebp_QL65_.jpg",
+    title: "It Ends With Us",
+  },
+  {
+    id: 4,
+    author: "Francesc Miralles and Hector Garcia",
+    img: "https://m.media-amazon.com/images/I/814L+vq01mL._AC_UY327_FMwebp_QL65_.jpg",
+    title: "Ikigai",
+  },
+  {
+    id: 5,
+    author: "Ryuu Shinohara",
+    img: "https://m.media-amazon.com/images/I/711oJhQugjL._AC_UY327_FMwebp_QL65_.jpg",
+    title: "The Magic Of Manifesting",
+  },
+  {
+    id: 6,
+    author: "Robin Sharma",
+    img: "https://m.media-amazon.com/images/I/618ZOX7UNNL._AC_UY327_FMwebp_QL65_.jpg",
+    title: "The 5 AM Club",
+  },
+];
